02eb7de3c2f44deb835dd7e23dcf980b6e6c2de5
ericslc <eric.leatham@gmail.com>
2016-02-15 17:44:05 -0700

    finished day 1

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,8 +1,15 @@
 angular.module('devMtIn')
 .controller('homeCtrl', function($scope) {
-$scope.myProfile = [
-  {
-    name: 'Eric',
+  $scope.myProfile = {
+       friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+  }
+  $scope.sortOptions = [{
+    display: 'Ascending'
+  , value: false
   },
-]
+  {
+    display: 'Descending'
+  , value: true
+  }
+];
 });
